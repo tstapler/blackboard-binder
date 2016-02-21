7c42b488e55a8b218ac4e496ac1d0d6081e9c90e
cstapler <cstapler@iastate.edu>
2016-02-21 03:31:57 -0600

    Changes appearance of web application

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField
+from wtforms import StringField, BooleanField,PasswordField
 from wtforms.validators import DataRequired
 
 class LoginForm(Form):
     username = StringField('username', validators=[DataRequired()])
-    password = StringField('password', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
