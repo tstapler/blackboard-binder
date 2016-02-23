8cc5f4f2df6dbc1fcb29f3922f177819c405cf18
Tyler Stapler <tstapler@iastate.edu>
2016-02-22 19:22:17 -0600

    Prunes unneeded files from repo

--- hello.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, request, url_for
-app = Flask(__name__)
-
-@app.route('/homepage')
-def hello_world(name=None):
-	return render_template('homepageNav.html', name=name)
-
-if __name__== '__main__':
-	app.run(debug=True)
