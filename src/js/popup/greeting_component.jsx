15e631af73b13afe28840dcb6de121aa2a2d46fb
Chris Stapler <cstapler@iastate.edu>
2017-12-19 12:28:36 -0800

    Add start to popup display

--- src/js/popup/greeting_component.jsx
+++ src/js/popup/greeting_component.jsx
@@ -2,11 +2,29 @@ import React from "react";
 import icon from "../../img/icon-128.png"
 
 export default class extends React.Component {
+
+  constructor(props) {
+        super(props);
+        this.state = {
+            list: Array(2).fill("hi")
+        }
+        this.handleList = this.handleList.bind(this);
+  }
+
+  handleList() {
+    this.setState(prevState => ({
+        list: Array(4).fill("hi")
+    }));
+    console.log(this);
+  }
+
   render () {
+    const listItems = this.state.list.map((item) => <li>{item}</li>);
     return (
       <div>
-        <p>Hello, find me on src/js/popup/greeting_component.jsx</p>
-        <img src={icon} />
+        <h1>Download List:</h1>
+        <ul> {listItems} </ul>
+        <button onClick={this.handleList}>download now</button>
       </div>
     )
   }
