f23753e4cf6e950452b28eb4f8ad034d322120a1
Tyler Stapler <tystapler@gmail.com>
2017-12-20 17:05:34 -0600

    Add additional middleware for logging and development

--- src/js/background.js
+++ src/js/background.js
@@ -1,10 +1,19 @@
 import '../img/icon-128.png'
 import '../img/icon-34.png'
+import { applyMiddleware, compose  } from 'redux'
+import { render } from 'react-dom'
 import {wrapStore} from 'react-chrome-redux'
 import { createStore } from 'redux'
 import reducer from './reducers'
+import DevTools from './containers/DevTools'
+import { createLogger } from 'redux-logger'
+import freeze from 'redux-freeze'
 
-const store = createStore(reducer) // a normal Redux store
+const store = createStore(reducer, compose(
+      applyMiddleware(freeze, createLogger()),
+      DevTools.instrument()
+    )) // a normal Redux store
+ 
 
 window.store = store
 
